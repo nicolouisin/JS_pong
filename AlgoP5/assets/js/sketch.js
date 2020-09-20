75c5d1846e8d08c56a8c6391313701c43f7f6c43
raptorrouge <raptorrouge@gmail.com>
2020-09-20 19:55:28 +0200

    Pong v3
    score, affichage et blocage du rectangle en bas de l'écran

--- AlgoP5/assets/js/sketch.js
+++ AlgoP5/assets/js/sketch.js
@@ -10,13 +10,14 @@ let verticale;
 let vy;
 let Lrectangle;
 let i=5;
+let score;
 
 function preload() {
 }
 
 function setup(){
 	taille_ecran=1000;
-	createCanvas(taille_ecran/1.5,taille_ecran/1.5);
+	createCanvas(taille_ecran,taille_ecran/1.5);
 	x=10; //taille de la balle
 	px=10;  //possition centre balle
 	pry=taille_ecran/1.5-20 ;
@@ -28,6 +29,7 @@ function setup(){
 	Lrectangle=50;
 	vie=5;
 	lvl=0;
+	score=0;
 }
 
 
@@ -54,17 +56,27 @@ function draw() {
 	}
 
 	//gestion du rebond en haut et sur le rectangle
-	if((y>=taille_ecran/1.5 || ((y>=mouseY&&y<=mouseY+15)&&(px+5>=mouseX && px+5<=mouseX+Lrectangle)) )&& verticale=="bas" ){
+	if((y>=taille_ecran/1.5 || ((y>=taille_ecran/1.5&&y<=taille_ecran/1.5+15)&&(px+5>=mouseX && px+5<=mouseX+Lrectangle)) )&& verticale=="bas" ){
 		verticale="haut";
 		if(!(px>=mouseX && px<=mouseX+Lrectangle)&&y>=taille_ecran/1.5){
 			vie=vie-1;
 	}
-		if((y>=mouseY&&y<=mouseY+15)&&(px+5>=mouseX && px+5<=mouseX+Lrectangle)){
+		if((px+5>=mouseX && px+5<=mouseX+Lrectangle)){
 			i--;
+			score=score+10+lvl*10;
 			if(i==0) {
 				lvl+=1;
 				vx += lvl;
 				i=lvl*10;
+				if(vie<=5){
+					vie+=1;
+				}
+				if(lvl%2==0){
+					vy+=lvl;
+				}
+				if(lvl%3&&Lrectangle>25){
+					Lrectangle-=1;
+				}
 			}
 		}
 	}
@@ -83,13 +95,36 @@ function draw() {
 		window.alert("vous avez perdu au niv "+lvl+"!! fait f5 puis appuie sur ok pour rejouer");
 	}
 
+	if(mouseX+Lrectangle>=taille_ecran/1.5){
+		mouseX=taille_ecran/1.5-Lrectangle;
+	}
+	if(mouseX<=0){
+		mouseX=0;
+	}
 
 	fill(255,255,255,255);
 	stroke(255,255,255,255);
 	ellipse(px,y,x,x);
-	rect(mouseX,mouseY,Lrectangle,2);
+	rect(mouseX,taille_ecran/1.5-4,Lrectangle,2);
 
-	rect(800,80,50,50);
+	rect(taille_ecran/1.5+1,0,5,taille_ecran/1.5);
+	fill(0);
+
+	//tableau score, vie, niveau et niveau suivant
+	rect(750,0,50,50);
+	rect(700,0,50,50);
+	rect(800,0,50,50);
+	rect(850,0,100,50);
+	stroke(0);
+	fill(255);
+	text("niveau",755,20);
+	text(lvl,760,40);
+	text("vie",705,20);
+	text(vie,710,40);
+	text("score",805,20);
+	text(score,810,40);
+	text("niveau suivant",855,20);
+	text(i,860,40);
 
 
 }
